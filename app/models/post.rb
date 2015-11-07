9c5cb82fb37cf99b96c6895ef15614d339a82858
JMARCHESENYC <justinmarchese11@gmail.com>
2015-11-07 14:15:25 -0500

    Nav Bar

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,3 +4,9 @@ class Post < ActiveRecord::Base
   has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end  
+
+post = Post.first  
+
+def index  
+ @posts = Post.all
+end  
